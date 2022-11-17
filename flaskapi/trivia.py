bec78c2ae71d17fb477e595328a2d12b71af0dd0
marlenabrammer <marlena.brammer@gmail.com>
2022-11-17 20:48:02 +0000

    Flask and Jinga

---
+++ flaskapi/trivia.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Alta3 APIs and HTML"""
+
+## best practice says don't use commas in imports
+# use a single line for each import
+from flask import Flask
+from flask import redirect
+from flask import url_for
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+## This is where we want to redirect users to
+@app.route("/success/<name>")
+def success(name):
+    return f"Welcome {name}\n"
+# This is a landing point for users (a start)
+@app.route("/") # user can land at "/"
+@app.route("/start") # or user can land at "/start"
+def start():
+    return render_template("postmaker.html") # look for templates/postmaker.html
+# This is where postmaker.html POSTs data to
+# A user could also browser (GET) to this location
+@app.route("/answer", methods = ["POST", "GET"])
+def login():
+    # POST would likely come from a user interacting with postmaker.html
+    if request.method == "POST":
+        if request.form.get("nm"): # if nm was assigned via the POST
+            answer = request.form.get("nm") # grab the value of nm from the POST
+            if answer = '50 meters':
+                redirect(url_for("correct", answer = trivia_answer))
+            elif:
+
+    return redirect(url_for("success", name = user)) # pass back to /success with val for name
+if __name__ == "__main__":
+   app.run(host="0.0.0.0", port=2224) # runs the application
+
